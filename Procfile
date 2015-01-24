355427e01b15fc0f8b3c1b9087a8174b29ea0b28
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-01-24 17:15:17 -0300

    Heroku config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh -c 'cd ./var_log_recetas/ && gunicorn --log-file - var_log_recetas.wsgi'
--- var_log_recetas/var_log_recetas/wsgi.py
+++ var_log_recetas/var_log_recetas/wsgi.py
@@ -17,6 +17,8 @@ import os
 from os.path import abspath, dirname
 from sys import path
 
+from dj_static import Cling
+
 SITE_ROOT = dirname(dirname(abspath(__file__)))
 path.append(SITE_ROOT)
 
@@ -30,7 +32,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "var_log_recetas.settings.produc
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
