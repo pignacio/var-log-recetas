370444f9a785b98480e1f06e6840c11a47efed68
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-02-26 18:38:01 -0300

    editable ingredients

--- var_log_recetas/ingredient/views.py
+++ var_log_recetas/ingredient/views.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render, redirect
 from django.utils.translation import ugettext as _
-from django.views.generic import ListView, FormView
+from django.views.generic import ListView, FormView, UpdateView
 
 from .forms import IngredientForm, MeasureUnitForm
 from .models import Ingredient, MeasureUnit
@@ -23,6 +24,14 @@ class IngredientAddView(FormView):
     def form_valid(self, form):
         form.save()
         return redirect('ingredient_list')
+
+
+class IngredientUpdateView(UpdateView):
+    model = Ingredient
+    form_class = IngredientForm
+    template_name = 'generic/form.html'
+    pk_url_kwarg = 'ingredient_id'
+    success_url = reverse_lazy('ingredient_list')
 
 
 class MeasureUnitListView(ListView):
