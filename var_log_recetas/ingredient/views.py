79a0811c458ef550c4d806c4cfed8dd558a5e0e4
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-03-05 17:10:44 -0300

    Unit group views

--- var_log_recetas/ingredient/views.py
+++ var_log_recetas/ingredient/views.py
@@ -8,8 +8,8 @@ from django.utils.translation import ugettext as _
 from django.views.generic import ListView, FormView, UpdateView
 
 from utils.views import GenericFormMixin
-from .forms import IngredientForm, MeasureUnitForm
-from .models import Ingredient, MeasureUnit
+from .forms import IngredientForm, MeasureUnitForm, MeasureUnitGroupForm
+from .models import Ingredient, MeasureUnit, MeasureUnitGroup
 
 
 # Create your views here.
@@ -47,3 +47,24 @@ class MeasureUnitAddView(GenericFormMixin, FormView):
     def form_valid(self, form):
         form.save()
         return redirect('unit_list')
+
+
+class MeasureUnitGroupListView(ListView):
+    model = MeasureUnitGroup
+
+
+class MeasureUnitGroupAddView(GenericFormMixin, FormView):
+    form_class = MeasureUnitGroupForm
+    title = _("Agregar grupo de unidades")
+
+    def form_valid(self, form):
+        form.save()
+        return redirect('unit_group_list')
+
+
+class MeasureUnitGroupUpdateView(GenericFormMixin, UpdateView):
+    model = MeasureUnitGroup
+    form_class = MeasureUnitGroupForm
+    pk_url_kwarg = 'unit_group_id'
+    success_url = reverse_lazy('unit_group_list')
+    title = _("Modificar grupo de unidades")
